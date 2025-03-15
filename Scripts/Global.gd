f785a66e67bf3742e734d05666b4cfe8c183845e
Andrew Liu <andrewliu101720@gmail.com>
2025-03-14 21:52:27 -0600

    Slot machine ui integration
    - Integrated slot machine code with Global script structure and variables

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -4,7 +4,17 @@ extends Node
 enum SymbolID {BLANK, APPLE, BANANA, CHERRY, KIWI, WATERMELON, TOMATO, AVOCADO, PINEAPPLE, STRAWBERRY, MANGO}
 
 # Preload textures for slot symbols
-var apple_texture = preload("res://icon.svg")
+@onready var blank_texture = preload("res://icon.svg")
+@onready var apple_texture = preload("res://assets/7.png")
+@onready var banana_texture = preload("res://assets/Banana.png")
+@onready var cherry_texture = preload("res://assets/Cherry.png")
+
+@onready var texture_array: Array = [
+		blank_texture,
+		apple_texture,
+		banana_texture,
+		cherry_texture
+]
 
 var bank: int
 var goal: int
--- control.gd
+++ control.gd
@@ -8,14 +8,7 @@ extends Control
 	$VBoxContainer/HBoxContainer/Slot5
 ]
 
-@onready var spin_button = $VBoxContainer/SpinButton
-
-var symbols = ["cherry", "banana", "7"]
-
-func _ready():
-	spin_button.pressed.connect(_on_spin_pressed)
-
-func _on_spin_pressed():
+func _on_spin_button_pressed():
 	for slot in slot_displays:
-		var chosen_symbol = symbols.pick_random()
-		slot.texture = load("res://assets/%s.png" % chosen_symbol)
+		var chosen_symbol = randi_range(0, 3) #len(Global.SymbolID) - 1)
+		slot.texture = Global.texture_array[chosen_symbol]
