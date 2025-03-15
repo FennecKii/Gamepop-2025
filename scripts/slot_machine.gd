5fcd01b24c9d12e2e665ef88ace40be5c2d18a26
imbretty <114277284+imbrettirl@users.noreply.github.com>
2025-03-15 14:37:17 -0600

    Money Functionality Added

--- scripts/slot_machine.gd
+++ scripts/slot_machine.gd
@@ -1,5 +1,8 @@
 extends Control
 
+@onready var bet_money_label = $SpinButton/Label
+@onready var money_label = $PlayerMoney
+
 @onready var slot_displays = [
 	$VBoxContainer/HBoxContainer/Slot1,
 	$VBoxContainer/HBoxContainer/Slot2,
@@ -10,7 +13,11 @@ extends Control
 
 signal spin_pressed
 
+func _ready():
+	money_label.text = "Money: " + str(Global.player_money)
+
 func _on_spin_button_pressed():
+	
 	# Resets symbol data array
 	Global.slot_data = []
 	# Loops through each texture rect
@@ -23,3 +30,21 @@ func _on_spin_button_pressed():
 		Global.slot_data.append(chosen_symbol)
 	# Emits signal connects to main_scene.gd 
 	spin_pressed.emit()
+
+func money_update():
+	money_label.text = "Money: " + str(Global.player_money)
+	bet_money_label.text = "" + str(Global.bet_money)
+	
+
+func _on_minus_button_pressed():
+	if Global.bet_money >= 10:
+		Global.player_money += 10
+		Global.bet_money -= 10
+		money_update()
+		
+
+func _on_plus_button_pressed():
+	if Global.player_money >= 10:
+		Global.bet_money += 10
+		Global.player_money -= 10
+		money_update()
